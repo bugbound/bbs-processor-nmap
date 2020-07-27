0aa1d69a0b587f7e1e761d1036d2b5a8528aec14
John Doe <johndoe@example.com>
2020-07-27 23:21:29 +0100

    apisupport

--- processnmap.go
+++ processnmap.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "github.com/tomsteele/go-nmap"
-    
+    "strings"
     "fmt"
     "io/ioutil"
     "os"
+    "net/http"
 )
 
 
@@ -27,7 +28,8 @@ func main() {
         
         var ports = host.Ports
         for pi := range ports {
-            fmt.Println(ports[pi].PortId, " - ", ports[pi].State.State)
+            fmt.Println(ports[pi].PortId, " - ", ports[pi].State.State, " - ", ports[pi].Service.Name)
+            addServiceToApi(host.Addresses[0].Addr, ports[pi].PortId, ports[pi].State.State, ports[pi].Service.Name)
         }
         fmt.Println("")      
         
@@ -35,3 +37,22 @@ func main() {
     
 }
 
+func addServiceToApi(ip string, portid int, state string, name string) {
+    url := "http://servicestore-service:7010/api/service_store"
+    jsonbody := fmt.Sprintf(`{"ip":"%s","port":"%d","state":"%s","name":"%s"}`, ip, portid, state, name)
+    req, err := http.NewRequest("POST", url, strings.NewReader(jsonbody))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    //fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+    
+}
